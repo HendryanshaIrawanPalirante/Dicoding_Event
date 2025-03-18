102edb54034f11b6217d063b8dd7763491497fa6
Hendryansha_Palirante <hendryanshapalirante@gmail.com>
2025-03-18 11:09:17 +0700

    Menambah loading test

--- app/src/main/java/com/example/dicodingevent/data/Result.kt
+++ app/src/main/java/com/example/dicodingevent/data/Result.kt
@@ -4,5 +4,4 @@ sealed class Result<out R> private constructor() {
     data class Success<out T>(val data: T) : Result<T>()
     data class Error(val error: String) : Result<Nothing>()
     data object Loading : Result<Nothing>()
-    data object LoadingTest : Result<Nothing>()
 }
